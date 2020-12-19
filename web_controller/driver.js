85dfc72e145fd68f0c43b179f32b8b7cc0436646
Delta <davidjoshuasim@gmail.com>
2020-12-19 15:52:43 -0800

    js

--- web_controller/driver.js
+++ web_controller/driver.js
@@ -1,4 +1,40 @@
 var rpio = require('rpio');
 
-rpio.open(15, rpio.INPUT);
-console.log('Pin 15 is currently ' + (rpio.read(15) ? 'high' : 'low'));
+const PIN12 = 12;
+const PIN13 = 13;
+
+// Setup PWM pins
+rpio.open(PIN12, rpio.PWM);
+rpio.open(PIN13, rpio.PWM);
+rpio.pwmSetClockDivider(64); // Set PWM refresh rate to 19.2MHz/64=300kHz
+rpio.pwmSetRange(0, 1024); // PWM Range
+
+function stop() {
+    rpio.pwmSetData(PIN12, 0);
+    rpio.pwmSetData(PIN13, 0);
+}
+
+function drive(direction) {
+    switch (direction) {
+        case "forward":
+            rpio.pwmSetData(PIN12, 0);
+            rpio.pwmSetData(PIN13, 0);
+            break;
+        case "left":
+            rpio.pwmSetData(PIN12, 0);
+            rpio.pwmSetData(PIN13, 50);
+            break;
+        case "right":
+            rpio.pwmSetData(PIN12, 50);
+            rpio.pwmSetData(PIN13, 0);
+            break;
+        case "reverse":
+            console.log("Not Implemented!");
+            rpio.pwmSetData(PIN12, 0);
+            rpio.pwmSetData(PIN13, 0);
+            break;
+        default:
+            console.log("Unknown Direction");
+            break;
+    }
+}
