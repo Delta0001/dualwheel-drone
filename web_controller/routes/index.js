c9a37b1c4b4d4977d4da8ca8b815fb8d1577138d
Delta <davidjoshuasim@gmail.com>
2020-12-19 16:24:25 -0800

    js

--- web_controller/routes/index.js
+++ web_controller/routes/index.js
@@ -7,29 +7,36 @@ router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.post('/stop', function (req, res) {
+router.post('/stop', function (req, res, next) {
   res.send('Got a PUT request at /stop')
+  next()
+}, function (req, res) {
   driver.stop();
 })
 
-router.post('/forward', function (req, res) {
+router.post('/forward', function (req, res, next) {
   res.send('Got a PUT request at /forward')
+}, function (req, res) {
   driver.drive("forward");
 })
 
-router.post('/left', function (req, res) {
+router.post('/left', function (req, res, next) {
   res.send('Got a PUT request at /left')
+}, function (req, res) {
   driver.drive("left");
 })
 
-router.post('/right', function (req, res) {
+router.post('/right', function (req, res, next) {
   res.send('Got a PUT request at /right')
+}, function (req, res) {
   driver.drive("right");
 })
 
-router.post('/reverse', function (req, res) {
+router.post('/reverse', function (req, res, next) {
   res.send('Got a PUT request at /reverse')
+}, function (req, res) {
   driver.drive("reverse");
 })
+
 
 module.exports = router;
