43752fe3016ad41c913dab87131e0a5f86bded86
Delta <davidjoshuasim@gmail.com>
2020-12-19 19:36:13 -0800

    js

--- web_controller/routes/index.js
+++ web_controller/routes/index.js
@@ -9,41 +9,42 @@ router.get('/', function (req, res, next) {
 
 router.put('/speed/:id', function (req, res, next) {
   driver.setSpeed(req.params.id)
+  // console.log(req.params.id)
   next()
 }, function (req, res) {
   res.send('Got a PUT request at /stop')
 })
 
 router.post('/stop', function (req, res, next) {
-  driver.stop();
+  driver.stop()
   next()
 }, function (req, res) {
   res.send('Got a POST request at /stop')
 })
 
 router.post('/forward', function (req, res, next) {
-  driver.drive("forward");
+  driver.drive("forward")
   next()
 }, function (req, res) {
   res.send('Got a POST request at /forward')
 })
 
 router.post('/left', function (req, res, next) {
-  driver.drive("left");
+  driver.drive("left")
   next()
 }, function (req, res) {
   res.send('Got a POST request at /left')
 })
 
 router.post('/right', function (req, res, next) {
-  driver.drive("right");
+  driver.drive("right")
   next()
 }, function (req, res) {
   res.send('Got a POST request at /right')
 })
 
 router.post('/reverse', function (req, res, next) {
-  driver.drive("reverse");
+  driver.drive("reverse")
   next()
 }, function (req, res) {
   res.send('Got a POST request at /reverse')
