423dec20e6691e4b96e44f255d9bcb4bf990fa6e
Delta <davidjoshuasim@gmail.com>
2021-02-01 19:34:19 -0800

    updated gpio

--- gpiotest.py
+++ gpiotest.py
@@ -3,12 +3,12 @@ from time import sleep
 
 pwm0 = gpiozero.PWMOutputDevice("GPIO12", True, 0)
 pwm0.on()
-pwm0.frequency = 8000
-pwm0.value = 0.5
+pwm0.frequency = 50
+pwm0.value = 0.25
 while True:
-    pass
-#    pwm0.off()
-#    sleep(1)
-#    pwm0.on()
-#    pwm0.frequency = 100
-#    sleep(1)
+    print("off")
+    pwm0.off()
+    sleep(1)
+    print("on")
+    pwm0.on()
+    sleep(1)
