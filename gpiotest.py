39a7b203d4b9a8132a3a353153a41ae406c31f8d
Delta <davidjoshuasim@gmail.com>
2021-02-01 20:36:12 -0800

    update

--- gpiotest.py
+++ gpiotest.py
@@ -14,12 +14,12 @@ print("0.0 ARM START")
 sleep(3)
 
 # arm
-pwm0.value = 0.03
+pwm0.value = 0.04
 print("0.03 ARM FINISH")
 sleep(5)
 
 print("RAMP to 7")
-x = 0.03
+x = 0.04
 while (x < 7.0):
     x += 0.01
     
