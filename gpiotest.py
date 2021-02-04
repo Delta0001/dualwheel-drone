01e6fe306ebfccebb3ae4c4172276020af6da6dc
Delta <davidjoshuasim@gmail.com>
2021-02-03 17:54:12 -0800

    push to prod

--- gpiotest.py
+++ gpiotest.py
@@ -4,22 +4,21 @@ from time import sleep
 pwm0 = gpiozero.PWMOutputDevice("GPIO12", active_high=True, initial_value=0, frequency=50)
 
 pwm0.off()
-print("off")
+print("POWER OFF")
 
 # power
 pwm0.on()
-# pwm0.value = 0.54
-# print("0.54 POWER ON")
+print("0.0 POWER ON")
 sleep(3)
 
 # arm sequence
-pwm0.value = 0.54
-print("0.54 ARM SEQ START")
+pwm0.value = 0.04
+print("0.04 ARM SEQ START")
 sleep(3)
 
-# pwm0.value = 0.54 #0.03 and 0.04 works for normal mode. 0.5 for bidirectional
-# print("0.5 ARM SEQ END")
-# sleep(5)
+pwm0.value = 0.54 #0.03 and 0.04 works for normal mode. 0.5 for bidirectional
+print("0.54 ARM SEQ END")
+sleep(5)
 
 # print("0.5")
 # pwm0.value = 0.5
