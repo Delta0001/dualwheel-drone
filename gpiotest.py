0d1bf7e727016f2656203fe4bfd69c9a607bdd39
Delta <davidjoshuasim@gmail.com>
2021-02-03 18:18:41 -0800

    push to prod

--- gpiotest.py
+++ gpiotest.py
@@ -13,13 +13,12 @@ sleep(5)
 
 # arm sequence
 print("ARM SEQ START")
-pwm0.value = 0.6
+pwm0.value = 0.59
 sleep(2)
-pwm0.value = 0.7
 
-print("ARM SEQ END")
-pwm0.value = 0.54 #0.03 and 0.04 works for normal mode. 0.5 for bidirectional
-sleep(5)
+# print("ARM SEQ END")
+# pwm0.value = 0.54 #0.03 and 0.04 works for normal mode. 0.5 for bidirectional
+# sleep(5)
 
 # print("0.5")
 # pwm0.value = 0.5
