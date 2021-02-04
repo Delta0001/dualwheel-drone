f8c55eb190a9414750c61cb25a3cf9e1a7ac1a0d
Delta <davidjoshuasim@gmail.com>
2021-02-03 17:26:24 -0800

    push to prod

--- gpiotest.py
+++ gpiotest.py
@@ -10,30 +10,22 @@ pwm0.on()
 print("on")
 
 pwm0.value = 0.0
-print("0.0 ARM START")
+print("0.0 ARM SEQ START")
 sleep(3)
 
 # arm
-pwm0.value = 0.04
-print("0.03 ARM FINISH")
+pwm0.value = 0.04 #0.03 seems to work too
+print("0.03 ARM SEQ END")
 sleep(5)
 
-print("RAMP to 7")
-x = 0.04
-while (x < 7.0):
-    x += 0.01
+pwm0.value = 0.5
+# print("RAMP to 7")
+# x = 0.04
+# while (x < 7.0):
+#     x += 0.01
     
-    pwm0.value = x
-    print(x)
-    sleep(0.1)
+#     pwm0.value = x
+#     print(x)
+#     sleep(0.1)
 
-# pwm0.value = 0.05
-# print("0.05")
-# sleep(5)
-
-# pwm0.value = 0.075
-# print("0.05")
-# sleep(5)
-
-# print("off")
 sleep(5)
