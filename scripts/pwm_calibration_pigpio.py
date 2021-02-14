a66d9c17c1feeb26f2f5342fd88da44fc45a9835
Delta <davidjoshuasim@gmail.com>
2021-02-13 16:33:29 -0800

    prod

--- scripts/pwm_calibration_pigpio.py
+++ scripts/pwm_calibration_pigpio.py
@@ -3,7 +3,9 @@ from gpiozero import Servo
 from time import sleep
 
 # factory = PiGPIOFactory()
+print("POWER ON")
 pwm0 = Servo("GPIO12", initial_value=0.0, min_pulse_width=1/1000, max_pulse_width=2/1000)
+sleep(4)
 
 print("MAX THROTTLE")
 pwm0.max()
