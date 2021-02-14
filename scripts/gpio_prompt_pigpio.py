39366359a9d5b16e50426f97d044e86a9108ef1c
Delta <davidjoshuasim@gmail.com>
2021-02-13 18:56:08 -0800

    YOU HAPPY NOW KOKO?

--- scripts/gpio_prompt_pigpio.py
+++ scripts/gpio_prompt_pigpio.py
@@ -7,30 +7,25 @@ pwm0 = Servo("GPIO12", initial_value=0.0, min_pulse_width=1/1000, max_pulse_widt
 pwm1 = Servo("GPIO13", initial_value=0.0, min_pulse_width=1/1000, max_pulse_width=2/1000, pin_factory=factory)
 
 while (1):
-    x = input("Enter Prompt: ")
+    x = input("Enter Prompt: ").lower()
 
     if (x == "+"):
         pwm0.value += 0.01
         pwm1.value += 0.01
-        print(pwm0.value)
-        print(pwm1.value)
+        print(pwm0.value, pwm1.value, sep=" - ")
     elif (x == "-"):
         pwm0.value -= 0.01
         pwm1.value -= 0.01
-        print(pwm0.value)
-        print(pwm1.value)
+        print(pwm0.value, pwm1.value, sep=" - ")
     elif (x == "value"):
         print(pwm0.value)
         print(pwm1.value)
     elif (x == "pw"):
-        print(pwm0.pulse_width)
-        print(pwm1.pulse_width)
+        print(pwm0.pulse_width, pwm1.pulse_width, sep=" - ")
     elif (x == "fw"):
-        print(pwm0.frame_width)
-        print(pwm1.frame_width)
+        print(pwm0.frame_width, pwm1.frame_width, sep=" - ")
     elif (x == "isactive"):
-        print(pwm0.is_active)
-        print(pwm1.is_active)
+        print(pwm0.is_active, pwm1.is_active, sep=" - ")
     elif (x == "min"):
         pwm0.min() # actual min seems to be -0.27
         pwm1.min() # actual min seems to be -0.27
