d9f3f8e3d813e7a8ab3faa5f272ae5b3177495b1
Delta <davidjoshuasim@gmail.com>
2021-02-13 16:45:36 -0800

    prod

--- scripts/gpio_prompt_pigpio.py
+++ scripts/gpio_prompt_pigpio.py
@@ -3,7 +3,7 @@ from gpiozero import Servo
 from time import sleep
 
 # factory = PiGPIOFactory()
-pwm0 = Servo("GPIO12", initial_value=0.0, min_pulse_width=1/1000, max_pulse_width=2/1000)
+pwm0 = Servo("GPIO12")#, initial_value=0.0, min_pulse_width=1/1000, max_pulse_width=2/1000)
 
 while (1):
     x = input("Enter Prompt: ")
@@ -18,6 +18,8 @@ while (1):
         print(pwm0.value)
     elif (x == "pw"):
         print(pwm0.pulse_width)
+    elif (x == "isactive"):
+        print(pwm0.is_active)
     elif (x == "min"):
         pwm0.min()
     elif (x == "mid"):
