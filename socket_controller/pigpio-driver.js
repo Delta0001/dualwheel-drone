0d8a1b4c26a5700a3e508f4ebcbdef1c3d5e641b
Delta <davidjoshuasim@gmail.com>
2021-02-27 13:24:56 -0800

    prod

--- socket_controller/pigpio-driver.js
+++ socket_controller/pigpio-driver.js
@@ -10,7 +10,6 @@ var throttle = MID_THROTTLE_PW;
 const motor1 = new Gpio(12, {mode: Gpio.OUTPUT});
 const motor2 = new Gpio(13, {mode: Gpio.OUTPUT});
 
-console.log("THIS WORKED? "); // debug
 // new_speed is a value between 0 and 100 indicating throttle percentage
 function setSpeed(new_speed) {
     var new_throttle = (MAX_THROTTLE_PW - MID_THROTTLE_PW) * (newspeed * 0.01);
@@ -20,6 +19,11 @@ function setSpeed(new_speed) {
     }
     console.log("global speed set to " + speed); // debug
 }
+
+console.log("Intiializing");
+
+setTimeout(motor1.servoWrite(MID_THROTTLE_PW), 1000)
+setTimeout(motor1.servoWrite(MID_THROTTLE_PW), 1000)
 
 function stop() {
     motor1.servoWrite(MID_THROTTLE_PW);
