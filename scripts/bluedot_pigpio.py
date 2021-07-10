811e9f9e9795d2a2ac2000c9a21db29c4917d37d
Delta <davidjoshuasim@gmail.com>
2021-07-10 15:57:23 -0700

    push

--- scripts/bluedot_pigpio.py
+++ scripts/bluedot_pigpio.py
@@ -6,29 +6,30 @@ from time import sleep
 
 bd = BlueDot()
 
+MID_THROTTLE = -0.055
+
 factory = PiGPIOFactory()
-pwm0 = Servo("GPIO12", initial_value=0.0, min_pulse_width=1/1000, max_pulse_width=2/1000, pin_factory=factory)
-pwm1 = Servo("GPIO13", initial_value=0.0, min_pulse_width=1/1000, max_pulse_width=2/1000, pin_factory=factory)
+PWM0 = Servo("GPIO12", initial_value=MID_THROTTLE, min_pulse_width=1/1000, max_pulse_width=2/1000, pin_factory=factory)
+PWM1 = Servo("GPIO13", initial_value=MID_THROTTLE, min_pulse_width=1/1000, max_pulse_width=2/1000, pin_factory=factory)
 
 speed = 0.05
-mid_throttle = -0.055
 
 def move(pos):
     if pos.top:
-        pwm0.value = float(mid_throttle+speed)
-        pwm1.value = float(mid_throttle+speed)
+        PWM0.value = float(MID_THROTTLE+speed)
+        PWM1.value = float(MID_THROTTLE+speed)
     elif pos.bottom:
-        pwm0.value = float(mid_throttle+speed)
-        pwm1.value = float(mid_throttle-speed)
+        PWM0.value = float(MID_THROTTLE-speed)
+        PWM1.value = float(MID_THROTTLE-speed)
     elif pos.left:
-        pwm0.value = float(mid_throttle-speed)
-        pwm1.value = float(mid_throttle-speed)
+        PWM0.value = float(MID_THROTTLE+speed)
+        PWM1.value = float(MID_THROTTLE-speed)
     elif pos.right:
-        pwm0.value = float(mid_throttle-speed)
-        pwm1.value = float(mid_throttle+speed)
+        PWM0.value = float(MID_THROTTLE-speed)
+        PWM1.value = float(MID_THROTTLE+speed)
 
 def stop():
-    pwm1.value = float(mid_throttle)
+    PWM1.value = float(MID_THROTTLE)
 
 bd.when_pressed = move
 bd.when_moved = move
