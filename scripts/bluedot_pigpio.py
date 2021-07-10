e7c8c426a672f65d79edcde340bbcd7ca884bad5
Delta <davidjoshuasim@gmail.com>
2021-07-10 15:54:15 -0700

    adjusted speeds

--- scripts/bluedot_pigpio.py
+++ scripts/bluedot_pigpio.py
@@ -10,24 +10,25 @@ factory = PiGPIOFactory()
 pwm0 = Servo("GPIO12", initial_value=0.0, min_pulse_width=1/1000, max_pulse_width=2/1000, pin_factory=factory)
 pwm1 = Servo("GPIO13", initial_value=0.0, min_pulse_width=1/1000, max_pulse_width=2/1000, pin_factory=factory)
 
-speed = 0.02
+speed = 0.05
+mid_throttle = -0.055
 
 def move(pos):
     if pos.top:
-        pwm0.value = float(speed)
-        pwm1.value = float(speed)
+        pwm0.value = float(mid_throttle+speed)
+        pwm1.value = float(mid_throttle+speed)
     elif pos.bottom:
-        pwm0.value = float(speed)
-        pwm1.value = float(speed)
+        pwm0.value = float(mid_throttle+speed)
+        pwm1.value = float(mid_throttle-speed)
     elif pos.left:
-        pwm0.value = float(speed)
-        pwm1.value = float(speed)
+        pwm0.value = float(mid_throttle-speed)
+        pwm1.value = float(mid_throttle-speed)
     elif pos.right:
-        pwm0.value = float(speed)
-        pwm1.value = float(speed)
+        pwm0.value = float(mid_throttle-speed)
+        pwm1.value = float(mid_throttle+speed)
 
 def stop():
-    pwm1.value = float(-0.2)
+    pwm1.value = float(mid_throttle)
 
 bd.when_pressed = move
 bd.when_moved = move
